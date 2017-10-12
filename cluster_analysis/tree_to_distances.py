e239eeac9c96d7b8fef6219dcce2a435789b033f
Sam Nooij <sam.nooij@rivm.nl>
2017-10-12 14:20:41 +0200

    corrected typos

--- cluster_analysis/tree_to_distances.py
+++ cluster_analysis/tree_to_distances.py
@@ -2,14 +2,15 @@
 
 #A script to calculate a pairwise distance matrix from a newick tree file.
 # Made by Will: https://www.biostars.org/p/55410/#55416
+# and slightly modified to correct typos.
 
 from Bio import Phylo
 from itertools import combinations
 
-tree = Phylo.readopentree_handle.name), 'newick')
+tree = Phylo.read("tree-file.nwk"), 'newick')
 seq_names = tree.get_terminals()
 dmat = {}
 for p1, p2 in combinations(seq_names, 2):
     d = tree.distance(p1, p2)
-    dmat[p1.name, p2.name)] = d
-    dmat[p2.name, p1.name)] = d
+    dmat[p1.name, p2.name] = d
+    dmat[p2.name, p1.name] = d
